c4c00e6fa6b37aade23e455378fed74da67ae2b4
gimhema <49871739+gimhema@users.noreply.github.com>
2023-08-13 00:54:50 +0900

    Modify RL Algorithm
    슈퍼클래스로부터 상속받는 방식에서 변경함

---
+++ RL/RLAlgorithm.cpp
@@ -0,0 +1,36 @@
+#include "RLAlgorithm.h"
+
+
+using namespace RLAlgo;
+
+DQN::DQN()
+{
+
+}
+
+DQN::~DQN()
+{
+
+}
+
+PPO::PPO()
+{
+
+}
+
+PPO::~PPO()
+{
+
+}
+
+SAC::SAC()
+{
+
+}
+
+SAC::~SAC()
+{
+
+}
+
+
--- RL/RLAlgorithm.h
+++ RL/RLAlgorithm.h
@@ -1,1 +1,31 @@
 #pragma once
+#include "NNet.h"
+#include "Utility.h"
+
+namespace RLAlgo
+{
+	class DQN
+	{
+	public:
+		DQN();
+		~DQN();
+	};
+
+
+	class PPO
+	{
+	public:
+		PPO();
+		~PPO();
+	};
+
+
+	class SAC
+	{
+	public:
+		SAC();
+		~SAC();
+	};
+}
+
+
