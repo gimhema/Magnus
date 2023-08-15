03bbd4a3221dd5a960e491df21af2e33b6f5fac4
gimhema <49871739+gimhema@users.noreply.github.com>
2023-08-15 16:17:06 +0900

    Modify Connector .cpp, stdafx.h
    IOCP 초기화 부분 로직 추가 및 에러 리턴용 enum을 별도로 선언

--- Network/Connector.cpp
+++ Network/Connector.cpp
@@ -22,7 +22,108 @@ int MagnusConnector::ConnectorMain()
 		return 1;
 	}
 
+	// Create Input/Output Completion Port Step
+	HANDLE hcp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+	if (hcp == NULL) return 1;
 
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+
+	HANDLE hTread;
+	for (int i = 0; i < (int)si.dwNumberOfProcessors * 2; ++i) {
+		hTread = CreateThread(NULL, 0, WorkerThread, hcp, 0, NULL);
+		if (hTread == NULL)
+		{
+			return 1;
+		}
+		CloseHandle(hTread);
+	}
+
+	// Create Socket Step
+	SOCKET listenSock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSock == INVALID_SOCKET)
+	{
+		// Error
+		return NET_ERROR::FAILED_CREATE_SOCKET;
+	}
+
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = bind(listenSock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR)
+	{
+		// Error
+		return NET_ERROR::FAILED_BIND_SOCKET;
+	}
+
+	retval = listen(listenSock, SOMAXCONN);
+	if (retval == SOCKET_ERROR)
+	{
+		// Error
+		return NET_ERROR::FAILED_LISTEN_SOCKET;
+	}
+
+	SOCKET clientSock;
+	struct sockaddr_in clientaddr;
+	int addrlen;
+	DWORD recvbytes, flags;
+
+	int state = NET_ERROR::SUCESS;
+
+	while (1)
+	{
+		addrlen = sizeof(clientaddr);
+		clientSock = accept(listenSock, (struct sockaddr*)&clientaddr, &addrlen);
+
+		if (clientSock == INVALID_SOCKET)
+		{
+			state = NET_ERROR::FAILED_INVALID_SOCKET;
+			break;
+		}
+
+		char addr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+		// Connect Client Successful
+
+
+		// Connect Client to I/O CompletionPort
+		CreateIoCompletionPort((HANDLE)clientSock, hcp, clientSock, 0);
+
+		SOCKETINFO* ptr = new SOCKETINFO;
+		if (ptr == NULL)
+		{
+			state = NET_ERROR::FAILED_INVALID_SOCKET;
+			break;
+		}
+
+		// Set Socket Information Struct
+		memset(&ptr->overlapped, 0, sizeof(ptr->overlapped));
+		ptr->socket = clientSock;
+		ptr->recvbytes = ptr->sendbytes = 0;
+		ptr->wsabuf.buf = ptr->buf;
+		ptr->wsabuf.len = BUFSIZE;
+
+		flags = 0;
+		retval = WSARecv(clientSock, &ptr->wsabuf, 1, &recvbytes,
+			&flags, &ptr->overlapped, NULL);
+
+		if (retval == SOCKET_ERROR) 
+		{
+			if (WSAGetLastError() != ERROR_IO_PENDING) 
+			{
+				state = NET_ERROR::FAILED_RECV;
+				
+			}
+			continue;
+		}
+	}
+
+	// End of Winsock
+	WSACleanup();
+	return 0;
 }
 
 
--- stdafx.h
+++ stdafx.h
@@ -4,6 +4,20 @@
 #include <Windows.h>
 #include <WinSock2.h>
 #include <stdio.h>
+#include <WS2tcpip.h>
 
 #define SERVERPORT 9000
 #define BUFSIZE 1024
+
+namespace NET_ERROR
+{
+	enum _KIND
+	{
+		SUCESS = 0,
+		FAILED_CREATE_SOCKET = 100,
+		FAILED_BIND_SOCKET = 101,
+		FAILED_LISTEN_SOCKET = 102,
+		FAILED_INVALID_SOCKET = 103,
+		FAILED_RECV = 104
+	};
+}
