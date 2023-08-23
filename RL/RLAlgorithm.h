44960109b3782614b09b85b14f230c65276cc42c
gimhema <49871739+gimhema@users.noreply.github.com>
2023-08-24 00:04:39 +0900

    Update RLAlgorithm.h
    .

--- RL/RLAlgorithm.h
+++ RL/RLAlgorithm.h
@@ -1,5 +1,50 @@
 #pragma once
+#include "../stdafx.h"
 #include "Utility.h"
+
+namespace RLSetup
+{
+	struct RLProperty
+	{
+		int NUM_ACTIONS;
+		int NUM_STATES;
+	};
+}
+
+namespace NNet
+{
+	struct NNetSetup
+	{
+		int BatchSize;
+		float LR;
+		float Gamma;
+		float Episilo;
+		int MemoryCapacity;
+		int QNetIteration;
+	};
+
+	struct Net : torch::nn::Module {
+
+		RLSetup::RLProperty rlInfo;
+		NNetSetup netInfo;
+
+		Net() {
+			fc1 = register_module("fc1", torch::nn::Linear(rlInfo.NUM_STATES, 50));
+			fc2 = register_module("fc2", torch::nn::Linear(50, 30));
+			out = register_module("out", torch::nn::Linear(30, rlInfo.NUM_ACTIONS));
+		}
+
+		torch::Tensor forward(torch::Tensor x) {
+			x = torch::relu(fc1->forward(x));
+			x = torch::relu(fc2->forward(x));
+			torch::Tensor action_prob = out->forward(x);
+			return action_prob;
+		}
+
+		torch::nn::Linear fc1{ nullptr }, fc2{ nullptr }, out{ nullptr };
+	};
+}
+
 
 namespace RLAlgo
 {
