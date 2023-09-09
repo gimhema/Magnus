f91c5102c776556f3260b1f4e47485ef8e2ef2a4
gimhema <49871739+gimhema@users.noreply.github.com>
2023-09-10 01:34:56 +0900

    .
    .

---
+++ PyWorkspace/ws_ray/rayServer/server.py
@@ -0,0 +1,18 @@
+
+
+
+class Server:
+    def __init__(self) -> None:
+        pass
+
+    def setup(self):
+        pass
+
+    def run(self):
+        pass
+
+    def recvMessageFromUnreal(self):
+        pass
+
+    def sendMessageToUnreal(self):
+        pass
---
+++ PyWorkspace/ws_ray/rayServer/unreal_env.py
@@ -0,0 +1,10 @@
+
+
+
+
+class UnrealEnv:
+    
+    def __init__(self):
+        pass
+
+
