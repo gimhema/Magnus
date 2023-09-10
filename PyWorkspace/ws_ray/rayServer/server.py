9b3cdf41e23c5c1bb70c3e932eae8889cd8c91a0
gimhema <49871739+gimhema@users.noreply.github.com>
2023-09-11 00:55:35 +0900

    Update server.py
    .

--- PyWorkspace/ws_ray/rayServer/server.py
+++ PyWorkspace/ws_ray/rayServer/server.py
@@ -1,5 +1,11 @@
+import ray
+import socket
+from ray.rllib.env.policy_server_input import PolicyServerInput
 
+ray.init()
 
+server_ip = "127.0.0.1"
+server_port = 9999
 
 class Server:
     def __init__(self) -> None:
@@ -8,7 +14,22 @@ class Server:
     def setup(self):
         pass
 
+    @ray.remote
     def run(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind((server_ip, server_port))
+            server_socket.listen()
+
+            print(f" Wait Server . . . {server_ip}:{server_port}.")
+
+            while True:
+                conn, addr = server_socket.accept()
+                with conn:
+                    print(f"Connected . . : {addr}")
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print(f"Recv data : : {data.decode('utf-8')}")
         pass
 
     def recvMessageFromUnreal(self):
