f4edc2a2b7558f73edc5b75234542e48263d56d3
gimhema <49871739+gimhema@users.noreply.github.com>
2023-09-12 23:37:59 +0900

    Update unreal_env.py

--- PyWorkspace/ws_ray/rayServer/unreal_env.py
+++ PyWorkspace/ws_ray/rayServer/unreal_env.py
@@ -1,10 +1,23 @@
+import gymnasium as gym
+import ray
+from ray.rllib.algorithms import ppo, dqn, sac
+
+print("IMPORT COMPLETED")
 
 
 
+class UnrealEnv(gym.Env):
+    def __init__(self, env_config):
+        self.action_space = # <gym.Space>
+        self.observation_space = # <gym.Space>
+    def reset(self, seed, options):
+        return # <obs>, <info>
+    def step(self, action):
+        return # <obs>, <reward: float>, <terminated: bool>, <truncated: bool>, <info: dict>
 
-class UnrealEnv:
-    
-    def __init__(self):
+
+
+class RayTrainer:
+    def __init__(self) -> None:
+        self.unreal_env = # UnraelEnv
         pass
-
-
