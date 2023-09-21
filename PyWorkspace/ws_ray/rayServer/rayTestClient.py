931eb24b4ee3be147cce99652e49b8dd426a68fb
Kimmo <49871739+gimhema@users.noreply.github.com>
2023-09-21 13:28:38 +0900

    Update rayTestClient.py
    데이터 송/수신 비동기로 분리

--- PyWorkspace/ws_ray/rayServer/rayTestClient.py
+++ PyWorkspace/ws_ray/rayServer/rayTestClient.py
@@ -14,16 +14,12 @@ async def send_messages():
         while True:
             # 사용자로부터 메시지 입력
             message = input("Enter a message (or 'exit' to quit): ")
-            
+
             if message == "exit":
                 break
-            
+
             # 메시지를 서버로 전송
             client_sock.sendall(message.encode('utf-8'))
-
-            # 서버로부터 응답 받기
-#            data = client_sock.recv(1024)
-#            print(f"Received from server: {data.decode('utf-8')}")
 
     except KeyboardInterrupt:
         pass
@@ -32,8 +28,30 @@ async def send_messages():
         client_sock.close()
         print("Disconnected from server.")
 
+async def recv_responses(client_sock):
+    while True:
+        try:
+            data = await asyncio.to_thread(client_sock.recv, 1024)
+            print(f"Received from server: {data.decode('utf-8')}")
+        except Exception as e:
+            print(f"Error receiving data: {str(e)}")
+            break
+
 async def main():
-    await send_messages()
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_sock.connect((server_ip, server_port))
+    print("Connected to server.")
+
+    recv_task = asyncio.create_task(recv_responses(client_sock))
+
+    try:
+        await asyncio.gather(send_messages(), recv_task)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # 연결 종료
+        client_sock.close()
+        print("Disconnected from server.")
 
 if __name__ == "__main__":
     asyncio.run(main())
