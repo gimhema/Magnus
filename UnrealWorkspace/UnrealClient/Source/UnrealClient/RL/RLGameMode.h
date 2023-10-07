3d7dc3ccf245255724747d25e89850151e165168
gimhema <49871739+gimhema@users.noreply.github.com>
2023-10-08 01:35:46 +0900

    Create RLGameMode
    .

---
+++ UnrealWorkspace/UnrealClient/Source/UnrealClient/RL/RLGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RLGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALCLIENT_API ARLGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
